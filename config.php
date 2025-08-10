2aea654c76d159598084e574deb4f06e12c6c33b
kiki526167 <maulanarizky7964@gamil.com>
2025-08-10 17:45:54 +0700

    Modul 1 - Database, ERD, Login & Dashboard

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+// Konfigurasi Aplikasi
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'name' => 'kasir_app',
+        'user' => 'root', // sesuaikan dengan local
+        'pass' => '',     // sesuaikan dengan local
+        'charset' => 'utf8mb4'
+    ],
+    'app' => [
+        'base_url' => '/kasir-app/public'
+    ]
+];
